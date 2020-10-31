90e8118a4649613fc06ae33a79e749b5fe9e1f2c
Stefanos Gjotas <stefanos.gjotas@gmail.com>
2020-10-31 13:20:27 +0100

    Bemerkungen im Code

--- wetter-projekt/sketch.js
+++ wetter-projekt/sketch.js
@@ -8,10 +8,10 @@ let input, button;
 
 var gui;
 
+// Hier werden die Variablen der Regler definiert
 var wind_speed = 0;
 var wind_direction = 0;
 var temperature_color = 0;
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -36,6 +36,7 @@ function setup() {
 
 function draw() {
 
+  // Definition der Farbverläufe, von bis, inkl. Mapping (worauf bezieht es sicht, von bis Wert, von bis Anzeige der Farbe)
   let c1 = lerpColor(color('#1c4794'), color('#951c1c'), map(temperature, 0, 40, 0, 1)); // Reserve Farben
   let c2 = lerpColor(color('#77ff94'), color('#77fdff'), map(wind_direction, 0, 360, 0, 1));
   let c3 = lerpColor(color('#1c4794'), color('#951c1c'), map(temperature_color, 0, 30, 0, 1));
@@ -43,21 +44,25 @@ function draw() {
 
   background(21, 21, 21);
 
+  // Ab hier werden die Pfeile für die Windrichtung definiert
+
+  // Alle 150Px folgt ein Pfeil
   let steps = 150;
 
-  for (let x = 25; x <= windowWidth * 2; x += steps) {
-    for (let y = 30; y <= windowHeight * 2; y += steps) {
+  for (let x = 25; x <= windowWidth * 2; x += steps) { // Die X Achse des ersten Pfeiles beginnt bei 25Px
+    for (let y = 30; y <= windowHeight * 2; y += steps) { // Die Y Achse des ersten Pfeiles beginnt bei 30Px
 
+      // Erstellung des Pfeiles mit einer Grössenskala von 0.5, Farbfüllung c2 sowie Rotation sind abhängig von der Windrichtung (0 bis 360°)
       push();
       scale(0.5, 0.5);
       fill(c2);
       translate(x, y);
-      rotate(wind_direction);
+      rotate(wind_direction); // Dieser Wert reglet die Drehung durch den GUI Regler UND durch die Wetter API Daten
       beginShape();
-      vertex(0, -20);
+      vertex(0, -20); // Durch die Minuswerte wird der Pfeil so gesetzt, dass dessen Mitte genau am XY Punkt sind
       vertex(15, 20);
       vertex(0, 10);
-      vertex(-15, 20);
+      vertex(-15, 20); // Durch die Minuswerte wird der Pfeil so gesetzt, dass dessen Mitte genau am XY Punkt sind
       endShape(CLOSE);
       pop();
     }
@@ -66,14 +71,15 @@ function draw() {
   background(21, 21, 21, 100);
 
   rectMode(CENTER);
-  angle += windrad + 0.1;
+  angle += windrad + 0.1; // Die Drehung erfolgt durch den Wert Windrad (abgeleitet von der Windstärke). Ohne Wind dreht sich das Rad trotzdem um 0.1Px pro Frame
 
+  // Hier entsteht das Windrad, angefangen durch Push, und mit Translate neu ausgerichtet
   push();
   strokeWeight(5);
   noFill();
 
   translate(width / 3 + 50, height / 2);
-  rotate(-angle);
+  rotate(-angle); // Der MinusAngle bewirkt, dass sich das Rad umgekehrt dreht (realistischer)
 
   scale(2, 2);
 
@@ -145,6 +151,7 @@ function draw() {
 
   pop();
 
+  // Dieser Code definiert die Drehung durch den GUI Regler
   for (let i = 1; i <= wind_speed; i++) {
     angle = angle + 0.1;
   }
@@ -159,40 +166,41 @@ function draw() {
   endShape(CLOSE);
   pop();
 
+  // Der Code für die Textzeilen Name, Grad, Windrichtung und Windstärke
   fill(c2);
   noStroke();
   textSize(95);
   textAlign(LEFT, TOP);
   textFont('Impact');
-  text(name, windowWidth - 420, windowHeight / 2);
+  text(name, windowWidth - 420, windowHeight / 2); // die Variable name bezieht sich auf den Wert (Name) aus der Wetter API
 
   textSize(40);
   noFill();
   stroke(255);
   strokeWeight(1);
-  text(temperature_color, windowWidth - 420, 630) & text('C°', windowWidth - 370, 630);
-  text(kompass, windowWidth - 420, 680);
-  text(windstaerke, windowWidth - 420, 730) & text('h/km', windowWidth - 370, 730);
-  // text(windstaerke + 'h/km', windowWidth - 420, 730); // Alternative Schreibweise
+  text(temperature_color, windowWidth - 420, 630) & text('C°', windowWidth - 370, 630); // die Variable temperature_color bezieht sich auf den Wert (Temperatur) aus der Wetter API
+  text(kompass, windowWidth - 420, 680); // die Variable kompass bezieht sich auf den Wert (Windrichtung in Grad) aus der Wetter API
+  text(windstaerke, windowWidth - 420, 730) & text('h/km', windowWidth - 370, 730); // die Variable windstaerke bezieht sich auf den Wert (Wind Speed) aus der Wetter API
+  // text(windstaerke + 'h/km', windowWidth - 420, 730); // Alternative Schreibweise für den Text mit Leerschlag
   noStroke();
 
 }
 
 function gotWeather(weather) {
-  windstaerke = weather.current.wind_speed; // Angaben in km!
+  windstaerke = weather.current.wind_speed; // Angaben in km
   windrad = map(windstaerke, 0, 200, 0, 20); // in Rotationsgrad mappen. Bei 200stdkm dreht sich das Windrad nun 0.1 Grad weiter pro Frame
   console.log(windstaerke)
 
-  wind_direction = weather.current.wind_degree;
+  wind_direction = weather.current.wind_degree; // Angaben in Grad (0-360°)
   console.log(wind_direction)
 
-  kompass = weather.current.wind_dir;
+  kompass = weather.current.wind_dir; // Angaben in Buchstaben (N, S, W, E etc.)
   console.log(kompass)
 
-  name = weather.location.name;
+  name = weather.location.name; // Angaben in Buchstaben (zb. Athens)
   console.log(name)
 
-  temperature_color = weather.current.temperature; // Angaben in Grad!
+  temperature_color = weather.current.temperature; // Angaben in Grad Celsius
   console.log(temperature)
 
 }
